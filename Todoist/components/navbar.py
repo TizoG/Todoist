a618b58388b52d5173d8497332e7e67ab2c0bdab
TizoG <tirado114@hotmail.com>
2025-02-22 19:37:33 +0100

    estilos navbar

--- Todoist/components/navbar.py
+++ Todoist/components/navbar.py
@@ -6,15 +6,27 @@ def navbar() -> rx.Component:
         rx.hstack(
             rx.heading(
                 "Gestor de Tareas",
+                line_height="40px"
             ),
             rx.link(
                 "Acerca de",
                 href="#",
                 text_align="center",
                 line_height="30px",
+                text_decoration="none",
+                color="white",
+                background_color="black",
+                padding_y="5px",
+                padding_x="10px",
+                border_radius="5px",
+                _hover={
+                    "background_color": "gray"
+                }
             ),
             justify="between",
             items="center",
 
-        )
+        ),
+        border_bottom="1px solid black",
+        padding_bottom="10px",
     )
